3a2cc45c9bf102a19e13e8db6213ba24c47d3e61
Thamer <54635233+thamer-al@users.noreply.github.com>
2019-09-01 16:29:55 -0400

    Magic Eight Ball.js

---
+++ Magic Eight Ball.js
@@ -0,0 +1,35 @@
+let userName = 'Thamer'
+userName ? console.log(`Hello, ${userName}`) : console.log ("Hello!")
+let userQuestion = "Can you answer my question?"
+console.log(`The user asked: ${userQuestion}`);
+const randomNumber = Math.floor(Math.random() * 8);
+let eightBall = '';
+switch(randomNumber){
+   case 0:
+    eightBall = 'It is certain';
+    break;
+   case 1:
+    eightBall ='It is decidedly so';
+    break;
+   case 2:
+    eightBall ='Reply hazy try again';
+    break;
+   case 3:
+    eightBall ='Cannot predict now';
+    break;
+   case 4:
+    eightBall ='Do not count on it';
+    break;
+   case 5:
+    eightBall ='My sourcer say no';
+    break;
+   case 6:
+    eightBall ='Outlook not so good';
+    break;
+   case 7:
+    eightBall ='Sogns point to yes';
+    break;
+}
+
+console.log(eightBall);
+
